6c39dd266d59435272897033afe435f97fd7507c
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-16 09:31:53 +0300

    new file

--- reddit/settings.py
+++ reddit/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'reddit',
         'USER': 'moringa',
-    'PASSWORD':'jluseno',
+    'PASSWORD':'Access',
     }
 }
 
